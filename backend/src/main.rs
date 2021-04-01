0b342a76782c95d27113cef7932c64e1ebe68709
Rodolfo Moi de Oliveira <rodolfo.moi@b2wdigital.com>
2021-04-01 11:17:00 -0300

    chore: extract db namespace

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,23 +1,7 @@
 use actix_web::{middleware::Logger, App, HttpServer};
-use backend::handlers;
-use config;
-use deadpool_postgres::{self};
+use backend::{db, handlers};
 use dotenv::dotenv;
-use serde::Deserialize;
 use tokio_postgres::NoTls;
-
-#[derive(Debug, Deserialize)]
-struct Config {
-    pg: deadpool_postgres::Config,
-}
-
-impl Config {
-    pub fn from_env() -> Result<Self, config::ConfigError> {
-        let mut cfg = config::Config::new();
-        cfg.merge(config::Environment::new().separator("__"))?;
-        cfg.try_into()
-    }
-}
 
 // TODO: ver! https://github.com/actix/examples/tree/master/database_interactions/pg/src
 #[actix_web::main]
@@ -25,7 +9,7 @@ async fn main() -> std::io::Result<()> {
     dotenv().ok();
     env_logger::init();
 
-    let config = Config::from_env().unwrap();
+    let config = db::Config::from_env().unwrap();
     let pool = config.pg.create_pool(NoTls).unwrap();
 
     let client = pool.get().await.unwrap();

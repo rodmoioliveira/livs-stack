77ed7a14e5f08cedc2321daed7e9393dc2dba9a9
Rodolfo Moi de Oliveira <rodolfo.moi@b2wdigital.com>
2021-04-02 21:19:07 -0300

    update commit

--- backend/src/handlers.rs
+++ backend/src/handlers.rs
@@ -1,5 +1,6 @@
-use crate::models;
-use actix_web::{get, web, HttpResponse, Responder, Result};
+use crate::{errors, models};
+use actix_web::{get, web, Error, HttpResponse, Responder, Result};
+use deadpool_postgres::{Client, Pool};
 
 #[get("/")]
 async fn index() -> Result<String> {
@@ -11,32 +12,28 @@ async fn books() -> impl Responder {
     HttpResponse::Ok().json(models::Books {
         data: vec![
             models::Book {
-                id: 12,
-                isbn: "isbn".to_string(),
+                isbn: 12,
                 author: "author".to_string(),
                 title: "title".to_string(),
                 editor: "editor".to_string(),
                 description: "description".to_string(),
             },
             models::Book {
-                id: 12,
-                isbn: "isbn".to_string(),
+                isbn: 12,
                 author: "author".to_string(),
                 title: "title".to_string(),
                 editor: "editor".to_string(),
                 description: "description".to_string(),
             },
             models::Book {
-                id: 12,
-                isbn: "isbn".to_string(),
+                isbn: 12,
                 author: "author".to_string(),
                 title: "title".to_string(),
                 editor: "editor".to_string(),
                 description: "description".to_string(),
             },
             models::Book {
-                id: 12,
-                isbn: "isbn".to_string(),
+                isbn: 12,
                 author: "author".to_string(),
                 title: "title".to_string(),
                 editor: "editor".to_string(),
@@ -47,13 +44,15 @@ async fn books() -> impl Responder {
 }
 
 #[get("/book/{isbn}")]
-async fn book(web::Path(isbn): web::Path<String>) -> impl Responder {
-    HttpResponse::Ok().json(models::Book {
-        id: 12,
-        isbn,
-        author: "author".to_string(),
-        title: "title".to_string(),
-        editor: "editor".to_string(),
-        description: "description".to_string(),
-    })
+async fn book(
+    web::Path(isbn): web::Path<String>,
+    db_pool: web::Data<Pool>,
+) -> Result<HttpResponse, Error> {
+    let client: Client = db_pool.get().await.map_err(errors::MyError::PoolError)?;
+    let query: String = format!("SELECT * FROM books WHERE isbn = '{}'", isbn);
+    let stmt = client.prepare(&query).await.unwrap();
+    let rows = client.query(&stmt, &[]).await.unwrap();
+    let book: Vec<models::Book> = serde_postgres::from_rows(&rows).unwrap();
+
+    Ok(HttpResponse::Ok().json(book))
 }
--- db/init.sql
+++ db/init.sql
@@ -1,13 +1,18 @@
 CREATE TABLE books (
-    id BIGSERIAL NOT NULL PRIMARY KEY,
-    isbn VARCHAR(255) NOT NULL,
-    author VARCHAR(255) NOT NULL,
-    title VARCHAR(255) NOT NULL,
-    editor VARCHAR(255) NOT NULL,
-    description TEXT NOT NULL
+  isbn BIGSERIAL NOT NULL PRIMARY KEY,
+  author VARCHAR(255) NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  editor VARCHAR(255) NOT NULL,
+  description TEXT NOT NULL
 );
 
-INSERT INTO books(isbn, author, title, editor, description)
+INSERT INTO
+  books(isbn, author, title, editor, description)
 VALUES
-    ('B07M82PNSX', 'Bill Bryson', 'The Body: A Guide for Occupants', 'Anchor', 'Bill Bryson once again proves himself to be an incomparable companion as he guides us through the human body—how it functions, its remarkable ability to heal itself, and (unfortunately) the ways it can fail. Full of extraordinary facts (your body made a million red blood cells since you started reading this) and irresistible Brysonesque anecdotes, The Body will lead you to a deeper understanding of the miracle that is life in general and you in particular.'),
-    ('B07C6YRCGJ', 'Ryan Jacobs ', 'The Truffle Underground: A Tale of Mystery, Mayhem, and Manipulation in the Shadowy Market of the Worlds Most Expensive Fungus', 'Clarkson Potter', 'Beneath the gloss of star chefs and crystal-laden tables, the truffle supply chain is touched by theft, secrecy, sabotage, and fraud. Farmers patrol their fields with rifles and fear losing trade secrets to spies. Hunters plant poisoned meatballs to eliminate rival truffle-hunting dogs. Naive buyers and even knowledgeable experts are duped by liars and counterfeits.');
+  (
+    '9788525060600',
+    'Elena Ferrante',
+    'A amiga genial',
+    'Biblioteca Azul - Globo',
+    'Quando uma amiga decide desaparecer, a outra resolve contar sua história. A Amiga Genial é o convite para a descoberta do universo de Elena Ferrante- árido, tenso, delicado, profundo e, sobretudo, humano. Duas meninas, no subúrbio de Nápoles passam juntas pelas descobertas da infância, atravessam as turbulências da adolescência e encaram as primeiras convenções da vida adulta.'
+  );

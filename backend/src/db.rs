43a0cddaef9919fc2876d75fa9350373b9f9eebd
Rodolfo Moi de Oliveira <rodolfo.moi@b2wdigital.com>
2021-04-06 22:05:41 -0300

    feat: implements insert_title

--- backend/src/db.rs
+++ backend/src/db.rs
@@ -1,6 +1,8 @@
+use crate::{errors, models};
 use config;
-use deadpool_postgres;
+use deadpool_postgres::Client;
 use serde::Deserialize;
+use tokio_pg_mapper::FromTokioPostgresRow;
 
 #[derive(Debug, Deserialize)]
 pub struct Config {
@@ -13,4 +15,31 @@ impl Config {
         cfg.merge(config::Environment::new().separator("__"))?;
         cfg.try_into()
     }
+}
+
+pub async fn insert_title(
+    client: &Client,
+    title: models::Title,
+) -> Result<models::Title, errors::MyError> {
+    let _stmt = include_str!("./sql/insert_title.sql");
+    let _stmt = _stmt.replace("$table_fields", &models::Title::sql_table_fields());
+    let stmt = client.prepare(&_stmt).await.unwrap();
+
+    client
+        .query(
+            &stmt,
+            &[
+                &title.isbn,
+                &title.author,
+                &title.title,
+                &title.publisher,
+                &title.year,
+            ],
+        )
+        .await?
+        .iter()
+        .map(|row| models::Title::from_row_ref(row).unwrap())
+        .collect::<Vec<models::Title>>()
+        .pop()
+        .ok_or(errors::MyError::NotFound) // more applicable for SELECTs
 }

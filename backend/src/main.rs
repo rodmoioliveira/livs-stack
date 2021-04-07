fc38d2321a55548ba0062d1f6a7e0a43f390e0ca
Rodolfo Moi de Oliveira <rodolfo.moi@b2wdigital.com>
2021-04-07 16:01:22 -0300

    feat: implements json_cfg

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,5 +1,5 @@
-use actix_web::{middleware, App, HttpServer};
-use backend::{db, handlers};
+use actix_web::{error, middleware, web, App, HttpResponse, HttpServer};
+use backend::{db, errors, handlers};
 use dotenv::dotenv;
 use tokio_postgres::NoTls;
 
@@ -14,8 +14,21 @@ async fn main() -> std::io::Result<()> {
     let localhost = String::from("0.0.0.0:8081");
     println!("Server running in {}", localhost);
 
+    // https://actix.rs/actix-web/actix_web/web/struct.JsonConfig.html#method.error_handler
+    let json_cfg = web::JsonConfig::default()
+        .limit(4096)
+        .error_handler(|err, _req| {
+            let err_msg = err.to_string();
+            error::InternalError::from_response(
+                err,
+                HttpResponse::BadRequest().json(errors::JsonError::new(err_msg)),
+            )
+            .into()
+        });
+
     HttpServer::new(move || {
         App::new()
+            .app_data(json_cfg.clone())
             .data(db_pool.clone())
             .wrap(middleware::Logger::default())
             .wrap(middleware::Compress::default())

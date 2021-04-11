5bcb47caba99029cc81f8cc0a534460c6c369a4d
Rodolfo Moi de Oliveira <rodolfo.moi@b2wdigital.com>
2021-04-11 00:52:30 -0300

    chore: create queries namespace

---
+++ backend/src/db/config.rs
@@ -0,0 +1,15 @@
+use config;
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+pub struct Config {
+    pub pg: deadpool_postgres::Config,
+}
+
+impl Config {
+    pub fn from_env() -> Result<Self, config::ConfigError> {
+        let mut cfg = config::Config::new();
+        cfg.merge(config::Environment::new().separator("__"))?;
+        cfg.try_into()
+    }
+}

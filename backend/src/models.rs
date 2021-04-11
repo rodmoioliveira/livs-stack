8bae6aa0c36e864340f135f4c90f80531aae4fbc
Rodolfo Moi de Oliveira <rodolfo.moi@b2wdigital.com>
2021-04-10 23:14:03 -0300

    feat: change backend to reflect changes in db

--- backend/src/models.rs
+++ backend/src/models.rs
@@ -5,12 +5,17 @@ use tokio_pg_mapper_derive::PostgresMapper;
 #[pg_mapper(table = "titles")]
 pub struct Title {
     pub id: Option<i64>,
-    pub isbn: i64,
-    pub author: String,
+    pub isbn: String,
+    pub author: i64,
+    pub edition: i16,
+    pub format: String,
+    pub language: i64,
+    pub genre: i64,
+    pub pages: i16,
+    pub publisher: i64,
+    pub summary: String,
     pub title: String,
     pub year: i16,
-    pub genre_id: i64,
-    pub publisher_id: i64,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
--- backend/src/sql/insert_title.sql
+++ backend/src/sql/insert_title.sql
@@ -1,4 +1,28 @@
 INSERT INTO
-  titles(isbn, author, title, year, genre_id, publisher_id)
+  titles(
+     isbn
+    ,author
+    ,edition
+    ,format
+    ,language
+    ,genre
+    ,pages
+    ,publisher
+    ,summary
+    ,title
+    ,year
+  )
 VALUES
-  ($1, $2, $3, $4, $5, $6) RETURNING $table_fields;
+  (
+    $1
+   ,$2
+   ,$3
+   ,$4
+   ,$5
+   ,$6
+   ,$7
+   ,$8
+   ,$9
+   ,$10
+   ,$11
+  ) RETURNING $table_fields;

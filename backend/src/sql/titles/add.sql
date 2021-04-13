d133f9ee2712c393c28478e229dab6bb08fe3946
Rodolfo Moi de Oliveira <rodolfo.moi@b2wdigital.com>
2021-04-13 13:35:42 -0300

    chore: remove leading comma from sql query

--- backend/src/sql/titles/add.sql
+++ backend/src/sql/titles/add.sql
@@ -1,28 +1,16 @@
 INSERT INTO
   titles(
-     isbn
-    ,author
-    ,edition
-    ,format
-    ,language
-    ,genre
-    ,pages
-    ,publisher
-    ,summary
-    ,title
-    ,year
+    isbn,
+    author,
+    edition,
+    format,
+    language,
+    genre,
+    pages,
+    publisher,
+    summary,
+    title,
+    year
   )
 VALUES
-  (
-    $1
-   ,$2
-   ,$3
-   ,$4
-   ,$5
-   ,$6
-   ,$7
-   ,$8
-   ,$9
-   ,$10
-   ,$11
-  ) RETURNING $table_fields;
+  ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) RETURNING $table_fields;
--- backend/src/sql/titles/update.sql
+++ backend/src/sql/titles/update.sql
@@ -1,16 +1,16 @@
 UPDATE
   titles
 SET
-  isbn = $1
- ,author = $2
- ,edition = $3
- ,format = $4
- ,language = $5
- ,genre = $6
- ,pages = $7
- ,publisher = $8
- ,summary = $9
- ,title = $10
- ,year = $11
+  isbn = $1,
+  author = $2,
+  edition = $3,
+  format = $4,
+  language = $5,
+  genre = $6,
+  pages = $7,
+  publisher = $8,
+  summary = $9,
+  title = $10,
+  year = $11
 WHERE
   id = $12 RETURNING $table_fields;

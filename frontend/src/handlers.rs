166787ac3964680d985127fa94011881255a74d5
Rodolfo Moi de Oliveira <rodolfo.moi@b2wdigital.com>
2021-04-21 17:25:46 -0300

    wip

--- frontend/src/handlers.rs
+++ frontend/src/handlers.rs
@@ -1,2 +1,3 @@
 pub mod error;
 pub mod root;
+pub mod titles;
--- frontend/src/handlers/titles.rs
+++ frontend/src/handlers/titles.rs
@@ -1,17 +1,27 @@
 use crate::{errors, models};
 use actix_web::{web, HttpResponse};
 use handlebars::Handlebars;
+use reqwest::blocking::Client;
 
-pub async fn index(
+pub async fn all(
     hb: web::Data<Handlebars<'_>>,
+    client: web::Data<Client>,
     endpoints: web::Data<models::types::Endpoints>,
 ) -> Result<HttpResponse, errors::MyError> {
+    let res: serde_json::Value = client
+        .get(format!("{}/titles", endpoints.backend))
+        .send()
+        .map_err(errors::MyError::ReqwestError)?
+        .json()
+        .map_err(errors::MyError::ReqwestError)?;
+
     let data = serde_json::json!({
         // TODO: get within docker container for prod
         // https://docs.docker.com/compose/compose-file/compose-file-v3/#ipv4_address-ipv6_address
         "assets": endpoints.assets,
+        "titles": res["data"],
     });
 
-    let body = hb.render("index", &data).unwrap();
+    let body = hb.render("titles", &data).unwrap();
     Ok(HttpResponse::Ok().body(body))
 }

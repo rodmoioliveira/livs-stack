e8c93cb5cb97fcdbcbae43788acf101477475fa6
Rodolfo Moi de Oliveira <rodolfo.moi@b2wdigital.com>
2021-04-23 02:46:23 -0300

    feat: add fonts to assets

--- assets/src/main.rs
+++ assets/src/main.rs
@@ -1,3 +1,4 @@
+use actix_cors::Cors;
 use actix_files::Files;
 use actix_web::{middleware, App, HttpServer};
 use dotenv::dotenv;
@@ -12,7 +13,10 @@ async fn main() -> std::io::Result<()> {
     println!("Server running in {}", localhost);
 
     HttpServer::new(|| {
+        let cors = Cors::default().allow_any_origin().max_age(3600);
+        // https://stackoverflow.com/questions/65863107/how-to-set-expire-or-cache-control-header-when-serving-static-files-with-act
         App::new()
+            .wrap(cors)
             .wrap(middleware::Logger::default())
             .service(Files::new("/static", "static/").show_files_listing())
     })

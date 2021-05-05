1b845d9c5d1ab0aa88d817b0953dd5c4ae98d5bc
Rodolfo Moi de Oliveira <rodolfo.moi@b2wdigital.com>
2021-05-05 19:33:50 -0300

    fix: format sql query

--- backend/src/sql/titles/all.sql
+++ backend/src/sql/titles/all.sql
@@ -1,18 +1,16 @@
-WITH
-cte AS (
+WITH cte AS (
   SELECT
     *
   FROM
-    titles
-  $filters
-  )
-SELECT *
-FROM (
-  TABLE cte
-  $order_by
-  ) sub
-RIGHT JOIN (
-  SELECT count(*)
-  FROM cte
-) c(count)
-ON TRUE;
+    titles $filters
+)
+SELECT
+  *
+FROM
+  (TABLE cte $order_by) sub
+  RIGHT JOIN (
+    SELECT
+      count(*)
+    FROM
+      cte
+  ) c(count) ON TRUE;

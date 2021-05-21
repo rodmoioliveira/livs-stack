f2fb35ef58057a16acd62706aea6b9b546e1108b
Rodolfo Moi de Oliveira <rodolfo.moi@b2wdigital.com>
2021-05-21 17:41:38 -0300

    feat: add fake data to used book in inventory table

--- db/init.sql
+++ db/init.sql
@@ -224,6 +224,23 @@ SELECT
   RANDOM_INT(1,50)
 FROM GENERATE_SERIES(1, 10000) s(i);
 
+INSERT INTO inventory(
+  title_id,
+  price,
+  quantity,
+  used,
+  sku,
+  condition
+)
+SELECT
+  i,
+  RANDOM_INT(8,100),
+  RANDOM_INT(1,10),
+  TRUE,
+  CONCAT ('SKU_NEW_BOOK', '_', i),
+  RANDOM_TEXT(10)
+FROM GENERATE_SERIES(1, 10000) s(i);
+
 /* COPY inventory(id, title_id, price, quantity, used, sku, condition) */
 /* FROM */
 /*   '/csv/inventory.csv' DELIMITER ',' CSV HEADER; */

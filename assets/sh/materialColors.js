b800560e6f3d7f518e6a9785ede672bc33a4aee5
Rodolfo Moi de Oliveira <rodolfo.moi@b2wdigital.com>
2021-05-27 17:45:24 -0300

    chore: add some new material desing images

--- assets/sh/materialColors.js
+++ assets/sh/materialColors.js
@@ -5,11 +5,14 @@ const materialUiColors = [
     [...document.querySelectorAll('.color')]
       .filter(
         (a) =>
+          a.getAttribute('aria-label').indexOf('600') < 0 &&
           a.getAttribute('aria-label').indexOf('700') < 0 &&
           a.getAttribute('aria-label').indexOf('800') < 0 &&
           a.getAttribute('aria-label').indexOf('900') < 0 &&
           a.getAttribute('aria-label').indexOf('Brown') &&
-          a.getAttribute('aria-label').indexOf('Grey')
+          a.getAttribute('aria-label').indexOf('A200') &&
+          a.getAttribute('aria-label').indexOf('A300') &&
+          a.getAttribute('aria-label').indexOf('A400')
       )
       .map((e) =>
         e.getAttribute('value').replace('#', '').toLowerCase()
